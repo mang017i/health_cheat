cde053e93daf7277463ee96d54491b2b8ea249fb
Issam Hadjal <issam.hadjal@epitech.eu>
2023-03-07 09:44:30 +0100

    front and minions

---
+++ front/src/components/Tabs/Tabs.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Tabs.css";
+import CheatsIndex from "../CheatsIndex/CheatsIndex";
+import SearchInCategory from "../SearchInCategory/SearchInCategory";
+
+function TabsComponent() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabIndex) => {
+    setActiveTab(tabIndex);
+  };
+
+  return (
+    <div className="tabs_container">
+      <div className="tabs_nav">
+        <button onClick={() => handleTabClick(1)}>All Cheats</button>
+        <button onClick={() => handleTabClick(2)}>Bookmarks</button>
+      </div>
+      <div className="tabsContent">
+        {activeTab === 1 && (
+          <div>
+            <SearchInCategory />
+            <CheatsIndex />
+          </div>
+        )}
+        {activeTab === 2 && (
+          <div className="bookmark_container">
+            <SearchInCategory />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+export default TabsComponent;
